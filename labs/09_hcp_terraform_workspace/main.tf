7862453263e81af13b020bf5503f17ecdb6ca4bb
Gabriel Sollero <sollero.gms@gmail.com>
2026-07-09 19:12:31 -0300

    chore: add lab 9 initial files

---
+++ labs/09_hcp_terraform_workspace/main.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "terraform-course"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "public-subnet"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "private-subnet"
+    Environment = var.environment
+  }
+}
---
+++ labs/09_hcp_terraform_workspace/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform"
+  default_tags {
+    tags = {
+      Managed_By = "Terraform"
+      Project    = "Terraform Training"
+    }
+  }
+}
